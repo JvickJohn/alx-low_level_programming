c584301d363fac6a59a7e980011edfb50ba4051b
JvickJohn <godsonakor@gmail.com>
2022-10-13 17:28:21 -0700

    C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";61);
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n";6);
 	return(1)
 }
 
